b22051f95728c1a6472c3d8af651eb272f163679
Taha Shahid <tahashahid203@gmail.com>
2024-06-25 10:13:32 +0400

    This is a landing page for Nike shoes

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import PopularProducts from "./PopularProducts";
+import SuperQuality from "./SuperQuality";
+import Services from "./Services";
+import SpecialOffer from "./SpecialOffer";
+import CustomerReviews from "./CustomerReviews";
+import Footer from "./Footer";
+import Subscribe from "./Subscribe";
+
+export {
+    Hero, 
+    PopularProducts,
+    SuperQuality,
+    Services,
+    SpecialOffer,
+    CustomerReviews,
+    Footer,
+    Subscribe
+}
